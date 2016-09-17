7c5e4bc6f6d3de40eaa868f2670b4157f48b943c
PhuongND <ndp1100@gmail.com>
2016-09-17 12:37:12 +0700

    17/9

---
+++ UnityProject/Assets/Script/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = GameObject.Find("GameManager").GetComponent<GameManager>();
+            return _instance;
+        }
+
+        set { _instance = value; }
+    }
+
+    private bool choosingPrefab = false;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	        choosingPrefab = false;
+
+        MouseRaycast();
+	}
+
+    private void MouseRaycast()
+    {
+        if (choosingPrefab)
+        {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+            }
+            else {
+                Debug.Log("No hit");
+            }
+        }
+    }
+
+    public void SetPickedPrefab()
+    {
+        choosingPrefab = true;
+    }
+}
---
+++ UnityProject/Assets/Script/PrefabItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabItem : MonoBehaviour
+{
+
+    public GameObject Prefab;
+
+    private UIButton mBtn;
+	// Use this for initialization
+	void Start ()
+	{
+	    mBtn = gameObject.GetComponent<UIButton>();
+//	    if (mBtn)
+//	        mBtn.onClick.Add(new EventDelegate(OnClick));
+	}
+
+    void OnClick()
+    {
+        Debug.Log("On Click");
+        GameManager.instance.SetPickedPrefab();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
